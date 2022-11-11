037c18ec53249b9673ae0bd9779b83d807e06565
Alex Chan <alexander.m.chan@gmail.com>
2022-11-11 15:37:20 -0700

    move to src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [require('@alexmchan/preset-tailwind')],
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
